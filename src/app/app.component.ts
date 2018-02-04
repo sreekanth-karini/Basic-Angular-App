da210f1c482a8f917f6897eea424905da43cfc89
Sadhana <sadhanakamma@gmail.com>
2018-02-04 13:43:16 +0530

    Initial Project Submit
    - Strings,Numbers,Objects,Arrays.

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+	constructor(){
+		this.printingMessage('sadhana');
+	}
+  title = 'sample Application';
+  clickedcompany='';
+  clickednumber= 0;
+  clickedperson={};
+  companies = ["Hooper Labs","IBM","Infosys","HSBC","TCS","Tech Mahindra"];
+  numbers = [22,33,44,55,66,77];
+  details = {name:'sadhana', number:22};
+  pdetails = {name:'sreekanth',number:33};
+  classmates =[
+  {name:'sreekanth1',number:331},
+  {name:'sreekanth2',number:332},
+  {name:'sreekanth3',number:333}, 
+  {name:'sreekanth4',number:334},
+  {name:'sreekanth5',number:335},
+  {name:'sreekanth6',number:336}];
+  
+  printingMessage(name){
+  	console.log('Hello ' + name);
+  }
+  printingCompany(cmpnyname){
+  	this.clickedcompany= cmpnyname;
+  	console.log('Haii ' + cmpnyname);
+  }
+  printingNumber(number){
+  	this.clickednumber= number;
+  	console.log(number);
+  }
+  printingMember(member){
+  	this.clickedperson=member;
+  	console.log(member.name+' - '+member.number);
+  }
+ 
+
+
+}
+
